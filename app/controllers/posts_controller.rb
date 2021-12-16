6f2fa631a472a082a9a00942f329e7852b571725
Lingxiao Li <lingxiaoli98@gmail.com>
2021-12-15 22:55:30 -0500

    Add files via upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,39 @@
 class PostsController < ApplicationController
+
+
   def new
   end
 
   def index
-    @posts = Post.all
+    @posts = Post.all.with_attached_avatar
+    # $current = nil
+    if cookies.signed[:user_id].nil?
+      @current_user = $current
+      if @current_user.nil?
+        @current_user = "test_user"
+      end
+    else
+      @current_user = (User.find cookies.signed[:user_id])["user_name"]
+    end
   end
 
   def show
     id = params[:id]
+    if cookies.signed[:user_id].nil?
+      @current_user = $current
+      if @current_user.nil?
+        @current_user = "test_user"
+      end
+    else
+      @current_user = (User.find cookies.signed[:user_id])["user_name"]
+    end
     @post = Post.find(id)
-    @email = User.find_by_user_name(@post.post_by).email
+    if @post.post_by.nil? or User.find_by_user_name(@post.post_by).nil?
+      @email = "test@columbia.edu"
+    else
+      @email = User.find_by_user_name(@post.post_by).email
+    end
+#     @email = User.find_by_user_name(@post.post_by).email
   end
     
   def create
@@ -22,6 +46,7 @@ class PostsController < ApplicationController
       # attributes["email"] = ()
     end
     @post = Post.create!(attributes)
+    @post.avatar.attach(params[:post][:photo])
     flash[:notice] = "#{@post.title} was successfully created."
     redirect_to posts_path
   end
@@ -33,6 +58,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find params[:id]
     @post.update!(post_params)
+    @post.avatar.attach(params[:post][:photo])
     flash[:notice] = "#{@post.title} was successfully updated."
     redirect_to post_path(@post)
   end
@@ -43,12 +69,18 @@ class PostsController < ApplicationController
     flash[:notice] = "Post '#{@post.title}' deleted."
     redirect_to posts_path
   end
+
+  def userPosts
+    @user_name = params[:user_name]
+    @posts = Post.where(post_by: @user_name)
+  end
     
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.  
   def post_params
     params.require(:post).permit(:title, :price, :description, :post_by)
+    #to do avatart!
   end
 
 end
